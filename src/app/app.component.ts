9aa52bfc0aecc70dca7c90cbc6731e3c19d0e43a
ankitmishra23 <ankit23m071996@gmail.com>
2020-07-02 23:00:04 +0530

    week-2 day-4 angular task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'week2day3tasks';
+  price=["$0","$9","$49"];
+  data=[{heading:"Free",newprice:this.price[0],duration:"/month",user:"Single User",storage:"5GB Storage",project:"Unlimited Public Projects",access:"Community Access",privateproject:"Unlimited Private Projects",phone:"Dedicated Phone Support",domain:"Free Subdomain",reports:"Monthly Status Reports",button:"BUTTON",bold:false,show:false},
+  {heading:"Plus",newprice:this.price[1],duration:"/month",user:"5 User",storage:"50GB Storage",project:"Unlimited Public Projects",access:"Community Access",privateproject:"Unlimited Private Projects",phone:"Dedicated Phone Support",domain:"Free Subdomain",reports:"Monthly Status Reports",button:"BUTTON",bold:true,show:false},
+  {heading:"Pro",newprice:this.price[2],duration:"/month",user:"Unlimited User",storage:"150GB Storage",project:"Unlimited Public Projects",access:"Community Access",privateproject:"Unlimited Private Projects",phone:"Dedicated Phone Support",domain:"Free Subdomain",reports:"Monthly Status Reports",button:"BUTTON",bold:true,show:true}];  
 }
